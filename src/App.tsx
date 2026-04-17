cc3d97d8da1bc8d90c9a57373dcf523e9ce4203c
akash-chipsy <akash@chipsy.in>
2026-04-17 10:48:55 +0530

    optimized the fucntion

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,20 @@
 import { useState } from "react";
+import './App.css';
 
 function App() {
   const [count, setCount] = useState(0);
+
+  const handleIncrease = () => {
+    setCount(count + 1);
+  }
+
+  const handleDecrease = () => {
+    setCount(count - 1);
+  }
+
+  const handleReset = () => {
+    setCount(0);
+  }
 
   return (
     <div>
@@ -9,13 +22,13 @@ function App() {
       <p>Count: {count}</p>
 
       <div className="count-button">
-        <button onClick={() => setCount(count + 1)}>
+        <button onClick={handleIncrease}>
           Increase
         </button>
-        <button onClick={() => setCount(count - 1)}>
+        <button onClick={handleDecrease}>
           Decrease
         </button>
-        <button onClick={() => setCount(0)}>
+        <button disabled= {count === 0} onClick={handleReset}>
           Reset
         </button>
       </div>
